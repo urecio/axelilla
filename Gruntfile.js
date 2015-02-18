7f55cb608ff8b6293987848cbe400d5d39f2a037
Unai Recio <unai.recio@agcpartners.co.uk>
2015-02-18 20:07:24 +0000

    fix(build): - removes one useless jquery - Updates imagemin - Fixes regex for imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,7 +218,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= config.app %>/images',
-          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          src: ['**/*.{gif,jpeg,jpg,png}'],
           dest: '<%= config.dist %>/images'
         }]
       }
